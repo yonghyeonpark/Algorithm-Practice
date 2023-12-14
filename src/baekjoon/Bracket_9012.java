386cf70d48023604046441a0ab4a77fc4fa3631f
YongHyeon <pyh4616@naver.com>
2023-12-15 05:39:40 +0900

    백준 9012 - 괄호

---
+++ src/baekjoon/Bracket_9012.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bracket_9012 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        for (int i = 0; i < count; i++) {
+            String data = br.readLine();
+            while (true) {
+                if (!data.contains("()")) {
+                    break;
+                }
+                data = data.replaceAll("\\(\\)", "");
+            }
+            if (data.isEmpty()) {
+                System.out.println("YES");
+            }
+            if (!data.isEmpty()) {
+                System.out.println("NO");
+            }
+        }
+    }
+}
