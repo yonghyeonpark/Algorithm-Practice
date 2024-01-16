a55163c2517a3082ad88a3c39691ea40821b2303
yonghyeonpark <pyh4616@naver.com>
2024-01-16 20:20:36 +0900

    백준 11724 - 연결 요소의 개수

--- src/baekjoon/DFS와BFS/연결요소의개수_11724.java
+++ src/baekjoon/DFS와BFS/연결요소의개수_11724.java
@@ -1,8 +1,53 @@
 package baekjoon.DFS와BFS;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class 연결요소의개수_11724 {
 
-    public static void main(String[] args) {
+    private static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    private static boolean[] v;
+    private static int result;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        v = new boolean[n+1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        result = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!v[i]) {
+                dfs(i);
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static void dfs(int x) {
+        v[x] = true;
+        for (int i = 0; i < graph.get(x).size(); i++) {
+            int y = graph.get(x).get(i);
+            if (!v[y]) {
+                dfs(y);
+            }
+        }
     }
 }
